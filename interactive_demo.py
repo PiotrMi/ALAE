1f95a0cf80fd11fe62119b0c4827002fe38b7d0d
Adam Wankowski <apiotrw1@gmail.com>
2020-04-30 19:51:59 +0200

    dwajioj

--- interactive_demo.py
+++ interactive_demo.py
@@ -211,7 +211,7 @@ def sample(cfg, logger):
     #im = bimpy.Image(im)
     print("was")
     bild = im
-    display(Image(im))
+    display(Image.open(im))
     print("au hurr")
     test = Image.fromarray(bild)
     test.save("test.png")
